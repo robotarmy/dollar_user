223b3da182a72c70cc9386fd729181d2b79a269d
Curtis Schofield <178963+robotarmy@users.noreply.github.com>
2011-02-24 09:03:50 -0800

    workflow.zsh

--- workflow.zsh
+++ workflow.zsh
@@ -14,3 +14,32 @@ function git {
         ;;
   esac
 }
+
+function start_mongo_rs() {
+  mongod --replSet hvf --port $1 --dbpath ./$2 --quiet &
+}
+
+function start_mongo() {
+  mongod --port $1 --dbpath ./$2 --quiet &
+}
+
+function bu {
+  bundle update $*
+}
+
+function be {
+  bundle exec $*
+}
+
+function gits {
+  git status
+}
+
+function ccblank {
+ ssh -AY o_o@ec2-50-18-18-192.us-west-1.compute.amazonaws.com
+}
+
+function ccblankroot {
+ ssh -AY -i ~/.ssh/arun_aws_key.pem root@ec2-50-18-18-192.us-west-1.compute.amazonaws.com
+}
+
