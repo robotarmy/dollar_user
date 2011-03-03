c885811df22001a0f980b917dfe45c025413b616
Curtis & Danny <github@robotarmyma.de>
2011-03-02 16:38:16 -0800

    work rm

--- workflow.zsh
+++ workflow.zsh
@@ -16,7 +16,18 @@ function git {
   esac
 }
 
-alias rm='rm -i'
+if [ -n "$PS1" ] ; then
+rm () 
+{ 
+  ls -FCsd "$@"
+  echo 'remove[ny]? ' | tr -d '\012' ; read
+  if [ "_$REPLY" = "_y" ]; then
+    /bin/rm -rf "$@"
+  else
+    echo '(cancelled)'
+  fi
+}
+                                                            fi
 
 # Sensitive Material Crowdcast
 # For my eyes only
